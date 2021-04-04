d0aad65f34f062e307bf3771f13ce0010a4cce07
dyc <dyc@163.com>
2021-04-04 11:14:37 +0800

    分支的修改

--- src/main/java/com/dyc/git/HelloGit.java
+++ src/main/java/com/dyc/git/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hellp Git!");
 		System.out.println("这是新的分支");
-		System.out.println("这是dev分支的修改");
+		System.out.println("这是的修改");
 		System.out.println("这是master添加的");
 	}
 
