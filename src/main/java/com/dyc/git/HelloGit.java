ac6a0fb54e6ec77a197ad91aa581ee6aff09a26c
dyc <dyc@163.com>
2021-04-04 11:14:14 +0800

    主线的修改

--- src/main/java/com/dyc/git/HelloGit.java
+++ src/main/java/com/dyc/git/HelloGit.java
@@ -3,7 +3,7 @@ package com.dyc.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hellp Git!");
-		System.out.println("这是新的分支");
+		System.out.println("这是分支");
 		System.out.println("这是master添加的");
 		
 	}
